89f59486931155d1504058f1b0ab455b8b4ab52c
Haley Owen <haleyowen@utexas.edu>
2014-12-25 00:54:47 -0600

    initial commit for snake game

---
+++ SnakeGame/SnakeGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SnakeGame
+//
+//  Created by Haley Owen on 12/24/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
