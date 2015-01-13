95dad6acb39cbf78817389f701d9ae3ade3f08b9
Haley Owen <haleyowen@utexas.edu>
2015-01-13 15:23:55 -0600

    giving an example of moving a file from one directory to another in java

--- stackoverflow/MoveFile.java
+++ stackoverflow/MoveFile.java
@@ -1,11 +1,11 @@
 import java.io.File;
+
 public class MoveFile {
     public static void main(String[] args) {
+
 	try {
 	    File a = new File("C:\\folderA\\A.txt");
-
 	    a.renameTo(new File("C:\\folderB\\" + a.getName()));
-
 	    a.delete();
 	} catch(Exception e) {
 	    e.printStackTrace();
