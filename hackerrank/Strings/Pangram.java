7dc22f801d95f3dbdb5ee2956ca9293164216d64
Haley Owen <haleyowen@utexas.edu>
2015-02-01 23:03:23 -0600

    started strings in hackerrank and finished program to check for pangram

---
+++ hackerrank/Strings/Pangram.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Arrays;
+
+public class Pangram {
+  public static void main(String[]args) {
+    Scanner input = new Scanner(System.in);
+    Set<Character> alpha = new TreeSet<Character>();
+    String alphaStr = "abcdefghijklmnopqrstuvwxyz";
+
+    for(char x : alphaStr.toCharArray())
+      alpha.add(x);
+
+    Set<Character> letters = new TreeSet<Character>();
+    String line = input.nextLine().toLowerCase();
+
+    for(int i = 0; i < line.length(); i++) {
+      if(line.charAt(i) != ' ')
+        letters.add(line.charAt(i));
+    }
+
+    if(letters.equals(alpha))
+      System.out.println("pangram");
+    else
+      System.out.println("not pangram");
+  }
+}
