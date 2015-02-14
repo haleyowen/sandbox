2f4eb5179e49b3ba87d9053f304a2589b9e8b452
Haley Owen <haleyowen@utexas.edu>
2015-02-13 21:42:47 -0600

    complete Cavity hackerrank

--- hackerrank/Implementation/Cavity.java
+++ hackerrank/Implementation/Cavity.java
@@ -17,17 +17,14 @@ public class Cavity {
       for(int j = 1; j < size - 1; j++) {
         char cur = cavities[i][j];
         if(cavities[i-1][j] < cur && cavities[i+1][j] < cur && cavities[i][j+1] < cur && cavities[i][j-1] < cur) {
-          cavities[i][j] = 0;
+          cavities[i][j] = 'X';
         }
       }
     }
 
     for(int i = 0; i < size; i++) {
       for(int j = 0; j < size; j++) {
-        if(cavities[i][j] == 0)
-          System.out.print('X');
-        else
-          System.out.print(cavities[i][j]);
+        System.out.print(cavities[i][j]);
       }
       System.out.println();
     }
