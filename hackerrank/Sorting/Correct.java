bb40040c84b9e6f7f300e7244bfa3b86aab3e9dd
Haley Owen <haleyowen@utexas.edu>
2015-02-19 00:00:44 -0600

    fixed problem in hackerrank code

--- hackerrank/Sorting/Correct.java
+++ hackerrank/Sorting/Correct.java
@@ -7,13 +7,13 @@ public class Correct {
     for(int i = 1; i < A.length; i++){
       int value = A[i];
       int j = i - 1;
-      while(j > 0 && A[j] > value){
+      while(j >= 0 && A[j] > value){
         A[j + 1] = A[j];
         j = j - 1;
       }
       A[j + 1] = value;
     }
-    printArray(ar);
+    printArray(A);
   }
 
   static void printArray(int[] ar) {
