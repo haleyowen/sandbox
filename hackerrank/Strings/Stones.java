5dbb2c01391e7f9373b9a0d26c0274fcf6884620
Haley Owen <haleyowen@utexas.edu>
2015-02-25 21:10:32 -0600

    code is compiling

--- hackerrank/Strings/Stones.java
+++ hackerrank/Strings/Stones.java
@@ -1,13 +1,36 @@
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.HashSet;
+
 public class Stones {
   public static void main(String[]args) {
     Scanner input = new Scanner(System.in);
 
     int count = input.nextInt();
     input.nextLine();
+    
+    TreeMap<Character, Integer> stones = new TreeMap<Character, Integer>();
 
     for(int x = 0; x < count; x++) {
       String line = input.nextLine();
+      HashSet<Character> temp = new HashSet<Character>();
 
+      for(char i : line.toCharArray())
+        temp.add(i);
+
+      for(char i : temp) {
+        if(stones.containsKey(i))
+          stones.put(i, stones.get(i) + 1);
+        else 
+          stones.put(i, 1);
+      }
     }
+    int c = 0;
+
+    for(char i : stones.keySet()) {
+      if(stones.get(i) == count)
+        c++;
+    }
+    System.out.println(c);
   }
 }
