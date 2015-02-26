78dbdcd0479ce3ee643b97f7cc15a818b1634605
Haley Owen <haleyowen@utexas.edu>
2015-02-25 20:58:41 -0600

    started working on hackerrank Gem Stones

---
+++ hackerrank/Strings/Stones.java
@@ -0,0 +1,13 @@
+public class Stones {
+  public static void main(String[]args) {
+    Scanner input = new Scanner(System.in);
+
+    int count = input.nextInt();
+    input.nextLine();
+
+    for(int x = 0; x < count; x++) {
+      String line = input.nextLine();
+
+    }
+  }
+}
